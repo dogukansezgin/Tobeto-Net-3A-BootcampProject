8780d6f11b6770e3e5e7bb7ff00be996bed9b4c7
Doğukan Sezgin <dogukan_sezgin@hotmail.com>
2024-02-18 19:39:30 +0300

    repository fix

---
+++ DataAccess/Concretes/Repositories/EmployeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstracts;
+using DataAccess.Concretes.EntityFramework.Contexts;
+using Entities.Concretes;
+
+namespace DataAccess.Concretes.Repositories;
+
+public class EmployeeRepository : EfRepositoryBase<Employee, int, BaseDbContext>, IEmployeeRepository
+{
+    public EmployeeRepository(BaseDbContext context) : base(context)
+    {
+        
+    }
+}
