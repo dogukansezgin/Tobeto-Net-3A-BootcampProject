8e5d6c1f406cad959f83613476f50c38cfbd75e8
Doğukan Sezgin <dogukan_sezgin@hotmail.com>
2024-02-28 18:38:24 +0300

    Exceptions added.

---
+++ Core/Exceptions/Extensions/ProblemDetailsExtentions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace Core.Exceptions.Extensions;
+
+public static class ProblemDetailsExtentions
+{
+    public static string AsJson<TProblemDetail>(this TProblemDetail problemDetail)
+        where TProblemDetail : ProblemDetails => JsonSerializer.Serialize(problemDetail);
+}
